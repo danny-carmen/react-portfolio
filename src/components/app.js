4e08d53d0dcba392d1b2c964abbdf0412bc6b537
dannycarmen <danny-carmen@live.com>
2021-07-26 09:10:14 -0600

    Added portfolio containers

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
+
+
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Danny Carmen Portfolio</h1> 
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>       
+        
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div> 
+        <PortfolioContainer />
+          
+        
       </div>
     );
   }
